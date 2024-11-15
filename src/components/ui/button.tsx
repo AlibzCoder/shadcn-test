d1cf385ac6c5df058c02ac9545edffb3ba76f1f7
Ali Banai <abanaei@dblexchange.com>
2024-11-15 13:26:25 +0330

    created a base button and added loading feature to it

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,9 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot, Slottable } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -31,26 +32,68 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
+
+export interface BaseButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  leftEl?: React.ReactNode;
+  rightEl?: React.ReactNode;
+}
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
+  loading?: boolean;
+  hideContentOnLoading?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+const BaseButton = React.forwardRef<HTMLButtonElement, BaseButtonProps>(
+  (
+    {
+      children,
+      className,
+      variant,
+      size,
+      leftEl,
+      rightEl,
+      asChild = false,
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
-    )
+      >
+        {leftEl}
+        <Slottable>{children}</Slottable>
+        {rightEl}
+      </Comp>
+    );
   }
-)
-Button.displayName = "Button"
+);
+BaseButton.displayName = "BaseButton";
 
-export { Button, buttonVariants }
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    { children, loading = false, hideContentOnLoading = false, ...props },
+    ref
+  ) => {
+    const leftEl = loading ? <Loader2 className="animate-spin" /> : null;
+
+    return (
+      <BaseButton ref={ref} leftEl={leftEl} {...props}>
+        {loading && hideContentOnLoading ? null : children}
+      </BaseButton>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { BaseButton, Button, buttonVariants };
